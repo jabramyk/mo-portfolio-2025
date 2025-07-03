b4bee0a09dce0bd91a1b951ef7f3ed3c18d8c176
Mohamed Lamine Datt <161087278+MeeksonJr@users.noreply.github.com>
2025-07-02 18:35:41 -0700

    missed metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Meekson',
+  description: '2025 portfolio for Mohamed Datt, A.K.A MeeksonJr'
 }
 
 export default function RootLayout({
